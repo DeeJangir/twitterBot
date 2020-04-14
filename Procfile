5c89477cbfedca4fe35ec832998aceb36a010439
Deepak Jangir <deepakjangir4444@gmail.com>
2020-04-14 20:54:10 +0530

    add heroku support files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn --bind 0.0.0.0:$PORT engBot:app
+clock: python clock.py
---
+++ clock.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from requests import get
+sched = BlockingScheduler()
+from datetime import datetime
+
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     get("https://engbotpy.herokuapp.com/doTweet/")
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=12)
+def scheduled_job():
+    get("https://someapp.com/doTweet/")
+    print('i got hit at :- ' + str(datetime.now()))
+
+sched.start()
