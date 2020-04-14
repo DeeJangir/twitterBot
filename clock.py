5beac03c4af950a0069975164362e43a4da59223
Deepak Jangir <deepakjangir4444@gmail.com>
2020-04-14 21:06:53 +0530

    update files

--- clock.py
+++ clock.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 # @sched.scheduled_job('interval', minutes=1)
 # def timed_job():
-#     get("https://engbotpy.herokuapp.com/doTweet/")
+#     get("https://somesite.com/doTweet/")
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=12)
 def scheduled_job():
-    get("https://someapp.com/doTweet/")
+    get("https://somesite.com/doTweet/")
     print('i got hit at :- ' + str(datetime.now()))
 
 sched.start()
